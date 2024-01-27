ccc74d36187c525b17c9676a00de814d559ebae7
Ivan Mihaylov <ivan4o03.it@gmail.com>
2024-01-27 11:06:15 +0200

    Fix git ingore

---
+++ src/ImageStringify.Solution/ImageStringify.Sandbox/Program.cs
@@ -0,0 +1,17 @@
+﻿using ImageStringify.Lib;
+using System.Text;
+
+namespace ImageStringify.Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageStringifier = new ImageStringifier("C:\\Users\\Ivan Mihaylov\\Desktop\\41xsPjrM-pL._AC_UF894,1000_QL80_.jpg");
+
+            var imageAsString = imageStringifier.ToString();
+
+            File.WriteAllText("hello.txt", imageAsString);
+        }
+    }
+}
